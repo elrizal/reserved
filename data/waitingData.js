2c5fc11fbd51f1f93c33a23031881d4b4d01eeb0
elrizal <erin@lrizal.com>
2019-06-04 21:01:34 -0400

    basis

---
+++ data/waitingData.js
@@ -0,0 +1,8 @@
+const waitingData = [{
+    userName: 'Dude',
+    phoneNo: '000-000-0000',
+    userID: '0000',
+    emailID: 'name@name.com'
+}]
+
+module.exports = waitingData
---
+++ routing/apiRoutes.js
@@ -0,0 +1,25 @@
+require('../data/tableData.js')
+require('../data/waitingData.js')
+
+module.exports = function(app){
+    
+    app.get("/api/tables", function(req, res) {
+        res.json(tableData);
+      });
+      
+      app.get("/api/waiting", function(req, res) {
+        res.json(waitingData);
+      });
+    
+      app.post("api/tables", function(req, res){
+        if (tableData.length < 5) {
+            tableData.push(req.body);
+            res.json(true);
+          }
+          else {
+            waitListData.push(req.body);
+            res.json(false);
+          }
+      })
+
+}
